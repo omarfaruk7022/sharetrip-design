2b8fd018c692e247d265f43062d8ec3611f65fee
omarfaruk7022 <farhanfaruk221@gmail.com>
2024-05-27 05:37:04 +0600

    first section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
-import Navbar from "@/Components/Navbar";
 import Image from "next/image";
 import arrow from "@/assets/Vector.svg";
 import Link from "next/link";
+import ellipse from "@/assets/Ellipse.svg";
+import { IoArrowForwardSharp } from "react-icons/io5";
 
 export default function Home() {
   return (
@@ -28,13 +29,31 @@ export default function Home() {
             </Link>
           </div>
           <section className="bg-white w-[840px] h-[590px] rounded-[8px] my-4">
-            <div className="p-7">
-              <div>
-                <div className=" w-[24px]   h-[24px] bg-[#1882FF] text-[14px] font-semibold rounded-full ">
-                  <span className="flex justify-center ">1</span>
+            <div className="p-5 flex items-center justify-between border-b">
+              {" "}
+              <div className=" flex items-center gap-5">
+                <div className=" w-[24px]   h-[24px] bg-[#1882FF] text-[14px] font-semibold rounded-full flex justify-center items-center">
+                  <span className=" ">1</span>
+                </div>
+                <div className="">
+                  <p className="text-[#1A2B3D] font-semibold flex items-center gap-1">
+                    DAC <IoArrowForwardSharp />
+                    JFK
+                  </p>
+
+                  <p className="flex items-center gap-2 text-[#5A6573] text-[13px]">
+                    {" "}
+                    <span className=" ">Round Trip </span>
+                    <Image className="" src={ellipse} alt="" />
+                    <span>25 Mar - 4 Apr 2023 </span>
+                    <Image className="" src={ellipse} alt="" />
+                    <span>1 Stop</span>
+                  </p>
                 </div>
               </div>
-              <div></div>
+              <div className="bg-[#E8F3FF]  rounded-[32px]">
+                <span className="text-[#1882FF] text-[12px] px-2">33h 20m</span>
+              </div>
             </div>
           </section>
         </div>
